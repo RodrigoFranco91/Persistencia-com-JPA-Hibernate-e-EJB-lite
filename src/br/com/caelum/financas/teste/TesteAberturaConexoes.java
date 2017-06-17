82c230913af03f47874925304a411bf4b468d89e
Rodrigo Franco <rodrigofranco.ti@gmail.com>
2017-06-16 21:04:33 -0300

    Configurando e gerenciando o POOL com C3P0, ou seja, sem o DATA SOURCE

---
+++ src/br/com/caelum/financas/teste/TesteAberturaConexoes.java
@@ -0,0 +1,18 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteAberturaConexoes {
+
+	public static void main(String[] args) throws InterruptedException {
+		for (int i = 0; i < 30; i++) {
+			EntityManager manager = new JPAUtil().getEntityManager();
+			manager.getTransaction().begin();
+			System.out.println("Criado o EntityManager de número: " + i);
+		}
+
+		Thread.sleep(1000 * 30);
+	}
+}
